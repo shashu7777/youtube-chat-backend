5db43bad6ef3f7501c11a827d34489a109ee663a
shashu7777 <108219915+shashu7777@users.noreply.github.com>
2025-05-05 13:24:55 +0530

    Update server.py
    cannda proxy added

--- youtubeextension/server/server.py
+++ youtubeextension/server/server.py
@@ -23,11 +23,11 @@ def query():
     video_id = data.get("videoId")
 
     try:
-      # proxies = {
-      #  'http': 'http://47.251.122.81:8888',
-      #  'https': 'http://47.251.122.81:8888'
-      # }
-      transcript_list=YouTubeTranscriptApi.get_transcript(video_id=video_id,languages=['en','hi'])
+      proxies = {
+       'http': 'http://159.203.61.169:3128',
+       'https': 'http://159.203.61.169:3128'
+      }
+      transcript_list=YouTubeTranscriptApi.get_transcript(video_id=video_id,languages=['en','hi'], proxies=proxies)
       transcript=""
       for chunk in transcript_list:
           transcript=transcript+chunk['text']
